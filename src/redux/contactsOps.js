e3e15404883c3fa9844376cfc07a2f6a309330a4
vladislav-chihun <vladikkosmo@gmail.com>
2024-06-10 23:59:04 +0200

    items fix

--- src/redux/contactsOps.js
+++ src/redux/contactsOps.js
@@ -6,8 +6,8 @@ axios.defaults.baseURL = "https://6666f77ca2f8516ff7a5b0dc.mockapi.io"
 
 export const fetchContacts = createAsyncThunk("contacts/fetchAll", async (_,thunkAPI) => {
     try {
-        const response = axios.get(`/contacts`)
-        return response
+        const response = await axios.get(`/contacts`)
+        return response.data
     } catch (error) {
         return thunkAPI.rejectWithValue(error.message)
     } 
